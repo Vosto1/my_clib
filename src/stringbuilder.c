dbf2368d5ecf075b8ec63c48bea7a08e057becbe
Vosto1 <isac.hutchings34@gmail.com>
2025-05-04 18:19:01 +0200

    bugfix for stringbuilder.append (dstring/char*)

--- src/stringbuilder.c
+++ src/stringbuilder.c
@@ -4,7 +4,7 @@
 
 typedef enum
 {                               // decrease/increase memory results
-    MEM_DOUBLED = 0,            // doubled memory
+    MEM_INCREASED = 0,            // doubled memory
     MEM_HALVED = 1,             // memory halved
     MEM_OK = 2,                 // memory min size reached or no need
     ERR_MEM = 3,        // ERRor MEMory DECREASE
@@ -12,6 +12,7 @@ typedef enum
 
 static MEM sb_memory_decrease(stringBuilder *sb);
 static MEM sb_memory_increase(stringBuilder *sb);
+static MEM sb_memory_increase_manual(stringBuilder* sb, uint new_size);
 
 uint sb_length(stringBuilder* sb)
 {
@@ -43,7 +44,7 @@ bool sb_add(stringBuilder* sb, char c)
     }
 
     MEM m = sb_memory_increase(sb);
-    assert(m == MEM_OK || m == MEM_DOUBLED);
+    assert(m == MEM_OK || m == MEM_INCREASED);
 
     sb->string[sb->used] = c;
     sb->used += 1;
@@ -59,7 +60,13 @@ bool sb_append(stringBuilder* sb, dstring string)
     }
 
     MEM m = sb_memory_increase(sb);
-    assert(m == MEM_OK || m == MEM_DOUBLED);
+    assert(m == MEM_OK || m == MEM_INCREASED);
+
+    if (sb->used + string.length >= sb->size)
+    {
+        MEM m = sb_memory_increase_manual(sb, sb->used + string.length);
+        assert(m == MEM_INCREASED);
+    }
 
     for (uint i = sb->used; i < sb->used + string.length; i++)
     {
@@ -79,8 +86,14 @@ bool sb_appends(stringBuilder* sb, char* string)
     }
 
     MEM m = sb_memory_increase(sb);
-    assert(m == MEM_OK || m == MEM_DOUBLED);
+    assert(m == MEM_OK || m == MEM_INCREASED);
+
     int length = strlen(string);
+    if (sb->used + length >= sb->size)
+    {
+        MEM m = sb_memory_increase_manual(sb, sb->used + length);
+        assert(m == MEM_INCREASED);
+    }
 
     for (uint i = sb->used; i < sb->used + length; i++)
     {
@@ -121,20 +134,27 @@ static MEM sb_memory_increase(stringBuilder* sb)
     if (sb->used == sb->size - 1) // -1 because \0
     {   
         // when array is full on insert, double the size
-        sb->size *= 2;
-        char* temp = (char*)realloc(sb->string, sizeof(char) * sb->size);
-        if (temp != NULL)
-        {
-            sb->string = temp;
-            return MEM_DOUBLED;
-        }
-        else
-        {
-            return ERR_MEM;
-        }
+        return sb_memory_increase_manual(sb, sb->size * 2);
     }
     return MEM_OK;
 }
+
+static MEM sb_memory_increase_manual(stringBuilder* sb, uint new_size)
+{
+    // memory increase
+    char* temp = (char*)realloc(sb->string, sizeof(char) * new_size);
+    if (temp != NULL)
+    {
+        sb->size *= 2;
+        sb->string = temp;
+        return MEM_INCREASED;
+    }
+    else
+    {
+        return ERR_MEM;
+    }
+}
+
 // memory check + increase is done in arrayInsert function
 static MEM sb_memory_decrease(stringBuilder *sb)
 {
